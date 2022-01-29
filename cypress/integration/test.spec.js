21e9767a3952ab713bd8338deba37018c39ef18a
Jon Simon <jonathan_simon@yahoo.com>
2022-01-28 21:56:06 -0500

    Intercetped GET call to Google API

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -6,6 +6,87 @@ context('Search for Books', () => {
     })
   
     it('Search', () => {
+
+      const item = {
+        "kind": "books#volume",
+        "id": "eq9XvgAACAAJ",
+        "etag": "OUJlFaiST+A",
+        "selfLink": "https://www.googleapis.com/books/v1/volumes/eq9XvgAACAAJ",
+        "volumeInfo": {
+          "title": "Fantastic Beasts and Where to Find Them: Cinematic Guide: Newt Scamander Do Not Feed Out",
+          "authors": [
+            "Felicity Baker"
+          ],
+          "publishedDate": "2017-02-02",
+          "description": "The essential film companion for Newt Scamander! Relive the magic of Newt's world with this hardback guidebook featuring your favourite scenes and quotes from Fantastic Beasts and Where to Find Them.",
+          "industryIdentifiers": [
+            {
+              "type": "ISBN_10",
+              "identifier": "1407179403"
+            },
+            {
+              "type": "ISBN_13",
+              "identifier": "9781407179407"
+            }
+          ],
+          "readingModes": {
+            "text": false,
+            "image": false
+          },
+          "pageCount": 64,
+          "printType": "BOOK",
+          "averageRating": 5,
+          "ratingsCount": 1,
+          "maturityRating": "NOT_MATURE",
+          "allowAnonLogging": false,
+          "contentVersion": "preview-1.0.0",
+          "panelizationSummary": {
+            "containsEpubBubbles": false,
+            "containsImageBubbles": false
+          },
+          "imageLinks": {
+            "smallThumbnail": "http://books.google.com/books/content?id=eq9XvgAACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api",
+            "thumbnail": "http://localhost:3000/images/content.jpeg"
+          },
+          "language": "en",
+          "previewLink": "http://books.google.com/books?id=eq9XvgAACAAJ&dq=HarryPotte&hl=&cd=1&source=gbs_api",
+          "infoLink": "http://books.google.com/books?id=eq9XvgAACAAJ&dq=HarryPotte&hl=&source=gbs_api",
+          "canonicalVolumeLink": "https://books.google.com/books/about/Fantastic_Beasts_and_Where_to_Find_Them.html?hl=&id=eq9XvgAACAAJ"
+        },
+        "saleInfo": {
+          "country": "US",
+          "saleability": "NOT_FOR_SALE",
+          "isEbook": false
+        },
+        "accessInfo": {
+          "country": "US",
+          "viewability": "NO_PAGES",
+          "embeddable": false,
+          "publicDomain": false,
+          "textToSpeechPermission": "ALLOWED",
+          "epub": {
+            "isAvailable": false
+          },
+          "pdf": {
+            "isAvailable": false
+          },
+          "webReaderLink": "http://play.google.com/books/reader?id=eq9XvgAACAAJ&hl=&printsec=frontcover&source=gbs_api",
+          "accessViewStatus": "NONE",
+          "quoteSharingAllowed": false
+        },
+        "searchInfo": {
+          "textSnippet": "Relive all the magic of Newt&#39;s world with this hardback guidebook featuring all you need to know about Newt from the movie."
+        }
+      };
+
+      const response = {
+        items: [item],
+        kind: "books#volumes",
+        totalItems: 1
+      };
+
+       cy.intercept('GET','https://www.googleapis.com/books/v1/volumes?q=HarryPotte',response).as('Get Books');
+
        cy.get('#searchInput').type('Harry Potter');
        cy.get('.btn-search').click();
     })
